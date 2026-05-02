7ffc600b923d54e83d9be716ef945fb71caa82d0
adarshagc <adarshagc2004@gmail.com>
2026-05-02 20:16:38 +0530

    Initial commit

---
+++ src/main/java/com/example/userordersystem/UserordersystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.userordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserordersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserordersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userordersystem/UserordersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserordersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
